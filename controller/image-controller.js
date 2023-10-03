a2f42dfebbd329aad2becde0fed4c05fcd88adce
Rishi <rishimishra18052001@gmail.com>
2023-10-03 11:38:02 +0700

    images-issue fixed

--- controller/image-controller.js
+++ controller/image-controller.js
@@ -1,11 +1,21 @@
 import mongoose from 'mongoose';
-import { GridFSBucket } from 'mongodb';
 import dotenv from 'dotenv';
+import cloudinary from 'cloudinary';
 
 dotenv.config();
 
 const username = process.env.DB_USERNAME;
 const password = process.env.DB_PASSWORD;
+const cloudinaryApiKey = process.env.CLOUDINARY_API_KEY;
+const cloudinaryApiSecret = process.env.CLOUDINARY_API_SECRET;
+const cloudinaryCloudName = process.env.CLOUDINARY_CLOUD_NAME;
+
+// Configure Cloudinary with your credentials
+cloudinary.v2.config({
+  cloud_name: cloudinaryCloudName,
+  api_key: cloudinaryApiKey,
+  api_secret: cloudinaryApiSecret,
+});
 
 const url = "https://blogit-backend-s19u.onrender.com";
 const dbUrl = `mongodb+srv://${username}:${password}@cluster0.qj0tyeo.mongodb.net/?retryWrites=true&w=majority`;
@@ -21,12 +31,18 @@ conn.once('open', () => {
     });
 });
 
-export const uploadImage = (request, response) => {
+export const uploadImage = async (request, response) => {
     if (!request.file) return response.status(404).json('File not found');
 
-    const imageUrl = `${url}/file/${request.file.filename}`;
+    try {
+        const result = await cloudinary.v2.uploader.upload(request.file.path);
 
-    response.status(200).json(imageUrl);
+        const imageUrl = result.secure_url;
+
+        response.status(200).json(imageUrl);
+    } catch (error) {
+        response.status(500).json('Image upload failed');
+    }
 };
 
 export const getImage = (request, response) => {
